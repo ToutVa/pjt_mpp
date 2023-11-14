f438d171a227c64db5116938b2aac9011d4b539d
kjo7434 <kjo7434@naver.com>
2023-11-15 01:37:03 +0900

    누락한거

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -9,32 +9,51 @@ const Header = ({type}) => {
   
   const [profileStat , setProfileStat] = useRecoilState(loginStatus);
   
-  return (
-    <header className={type}>
-      <Link to={"/"}>
-        <div className="logo" />
-      </Link>
-      <nav>
-        <ul className="menu">
-          <li><a href="#">좋아요</a></li>
-          <li>
-            <Link to= {profileStat === true ? "/myPage": "/login"}>
-              <p className="linked-text">{profileStat === true ? "MyPage": "로그인"}</p>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className = "side-menu">
+  if("cover" === type) {
+    return (
+      <header className = "cover">
+        <Link to={"/"}>
+          <div className="cover-logo"/>
+        </Link>
+        <div className="flex">
+          <Link className="item" to= {profileStat === true ? "/myPage": "/login"}>
+                <p className="linked-text white">{profileStat === true ? "MyPage": "로그인"}</p>
+          </Link>
+          <Link className="item" to= {"/"}>
+                <p className="linked-text white">회원가입</p>
+          </Link>
+        </div>
+
+      </header>
+    )
+  }else {
+    return (
+      <header className={type}>
+        <Link to={"/"}>
+          <div className="logo"/>
+        </Link>
         <nav>
-          <ul>
-            <li> <a href="#"> mypage</a></li>
-            <li> <a href="#"> profile</a></li>
-            <li> <a href="#"> setting</a></li>
+          <ul className="menu">
+            <li><a href="#">좋아요</a></li>
+            <li>
+              <Link to= {profileStat === true ? "/myPage": "/login"}>
+                <p className="linked-text">{profileStat === true ? "MyPage": "로그인"}</p>
+              </Link>
+            </li>
           </ul>
         </nav>
-      </div>
-    </header>
-  );
+        <div className = "side-menu">
+          <nav>
+            <ul>
+              <li> <a href="#"> mypage</a></li>
+              <li> <a href="#"> profile</a></li>
+              <li> <a href="#"> setting</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
