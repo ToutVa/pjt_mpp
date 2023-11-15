a27de0f5e51feabf111fa3ff23ff80e6ed266568
kjo7434 <kjo7434@naver.com>
2023-11-16 01:25:38 +0900

    cover Page  기본디자인 완성

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,12 +6,23 @@ const Home = () => {
   return (
     <>
       <div className="cover-main">
-        <h2>지도 속 나를 기록하는 여행앱</h2>
+        <h1>지도 속 나를 기록하는 여행앱</h1>
+        <h5>My Pin Point</h5>
+        <div className="phone" />
       </div>
       <div className="cover-content">
-
+        <div className="item text">
+          <h3>맑은 하모니카 손에 익은 기타</h3>
+          <h3>Your melody (나는 떠날래)</h3>
+          <h3>어린왕자 your melody</h3>
+          <h3>찾아 떠날래~~~~</h3>
+        </div>
+        <div className="item img">
+          
+        </div>
       </div>
-      <h2>테스트 페이지</h2>
+
+      <div className="cover-arrow-down" />
     </>
   );
 };
