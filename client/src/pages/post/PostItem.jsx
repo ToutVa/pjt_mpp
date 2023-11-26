594e0d2ee1b7817890d4bd34e38a81f7175628c9
kjo7434 <kjo7434@naver.com>
2023-11-27 00:35:32 +0900

    post 메인 퍼블 1차완료

---
+++ client/src/pages/post/PostItem.jsx
@@ -0,0 +1,31 @@
+const PostItem = (props) => {
+    return (
+      <div className="item">
+        <div className="title-bar">
+            <div className="user" />
+            <div>
+                <div className="title">
+                    {props.content.title}
+                </div>
+                <div className="user-id">
+                  ec_asd
+                </div>
+            </div>
+        </div>
+        <div className="content">
+          {props.content.cont}
+        </div>
+        <div className="bottom">
+          <div className="icon-group">
+            <button className="like" />
+            <button className="comment" />
+          </div>
+          {props.content.like}
+        </div>
+      </div>
+    );
+    
+  };
+  
+  export default PostItem;
+  
