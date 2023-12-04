e4d04917fc9f17e587f8961dcf6d2430af70b16d
ec Jung <zv9612@gmail.com>
2023-12-04 22:04:17 +0900

    signup CSS 변경

--- client/src/pages/register/Login.jsx
+++ client/src/pages/register/Login.jsx
@@ -1,72 +1,65 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { useSetRecoilState } from "recoil";
-import { TokenUser } from "comm/recoil/TokenAtom";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { TokenUser } from 'comm/recoil/TokenAtom';
 import '../../css/login.css';
-import { useLocation, useNavigate } from "react-router";
-import TextBox from '../register/TextBox.jsx';
-import LoginButton from '../register/LoginButton.jsx';
-import RegisterButton from '../register/RegisterButton.jsx';
-
+import { useLocation, useNavigate } from 'react-router';
+import TextBox from '../register/TextBox';
+import SubmitButton from './SubmitButton';
+import LinkButton from './LinkButton';
 
 const Login = () => {
-    const [id, setId] = useState();
-    const [password, setPassword] = useState();
-    const setAccessToken = useSetRecoilState(TokenUser);
+  const [id, setId] = useState();
+  const [password, setPassword] = useState();
+  const setAccessToken = useSetRecoilState(TokenUser);
 
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const from = location?.state?.redirectedFrom?.pathname || "/feed";
+  const from = location?.state?.redirectedFrom?.pathname || '/feed';
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.post("/api/user/login", { id: id, password: password }).then((res) => {
-          const data = res.data;
-          if (data.resultMsg) {
-            setAccessToken(data.userData);
-            navigate(from);
-          } else {
-            alert(data.message);
-          }
-        }).catch((err) => {
-          console.log(err);
-          alert(err);
-        });
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('/api/user/login', { id: id, password: password })
+      .then((res) => {
+        const data = res.data;
+        if (data.resultMsg) {
+          setAccessToken(data.userData);
+          navigate(from);
+        } else {
+          alert(data.message);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err);
+      });
+  };
 
-    return (
-      <form className="login-main" onSubmit={handleSubmit}>
-        <div className="login-logo"/>
-          <TextBox 
-            id="inputId"
-            type="text" 
-            placeholder="아이디를 입력해주세요" 
-            onChange={(e) => {
-              setId(e.target.value);
-            }}
-          />
-          <TextBox 
-            id="inputPw"
-            type="password"
-            placeholder="패스워드를 입력해주세요" 
-            onChange={(e) => {
-              setPassword(e.target.value)
-            }}
-          />
-          <LoginButton 
-            label="L O G I N"
-            type="submit"
-            // onClick={() => {
-            //   handleSubmit();
-            // }}
-          /> 
-          <RegisterButton 
-            label="S I G N　U P"
-          />
-      </form>
-    )
-}
+  return (
+    <form className="login-main" onSubmit={handleSubmit}>
+      <div className="login-logo" />
+      <TextBox
+        id="inputId"
+        type="text"
+        placeholder="아이디를 입력해주세요"
+        onChange={(e) => {
+          setId(e.target.value);
+        }}
+      />
+      <TextBox
+        id="inputPw"
+        type="password"
+        placeholder="패스워드를 입력해주세요"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <SubmitButton label="L O G I N" type="submit" />
+      <LinkButton label="S I G N　U P" link="/sign" />
+    </form>
+  );
+};
 
 export default Login;
-  
--- client/src/pages/register/TextBox.jsx
+++ client/src/pages/register/TextBox.jsx
@@ -3,17 +3,17 @@ import '../../css/component.css';
 import '../../css/login.css';
 
 const TextBox = (e) => {
-    return (
-      <div className={'textBox'}>
-        <input className= "input"
-          id={e.id} 
-          type={e.type} 
-          value={e.value}
-          placeholder={e.placeholder}
-          onChange={e.onChange}
-        />
-      </div>
-    );
+  return (
+    <div className='textBox'>
+      <input className= 'input'
+        id={e.id} 
+        type={e.type} 
+        value={e.value}
+        placeholder={e.placeholder}
+        onChange={e.onChange}
+      />
+    </div>
+  );
 };
 
 export default TextBox;
