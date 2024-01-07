846b790c2b7ac66681e1970b726154440676e805
kjo7434 <kjo7434@naver.com>
2024-01-07 17:07:08 +0900

    tabControll 공통컴포넌트화

---
+++ client/src/component/TabController.jsx
@@ -0,0 +1,59 @@
+import "css/tabController.css";
+import {toggleClass, addClass, removeClass} from "comm/util";
+import { useState } from "react";
+
+const TabController = (props) => {
+  /**
+   *   탭으로 생성될 정보를 파라메터로 받아서 처리
+   * 
+   *                        header명         Content 에 들어갈 태그
+   *   ex) const menuArr =[{menu : "북마크", content: <Bookmark />},} ];
+   *       <TabController menuList={menuArr}/>
+   */
+
+  const menuArr =  props.menuList;
+  const [underlineWidth, setUnderlineWidth] = useState("");
+  const [underlineXpos, setunderlineXpos] = useState("");
+  
+  /*탭 onChange 이벤트*/
+  const fnTabOnChange = (index, e) => {
+
+    setUnderlineWidth(e.currentTarget.labels[0].offsetWidth);
+    setunderlineXpos(e.currentTarget.labels[0].offsetLeft);
+    menuArr.forEach((arr,idx) => {
+        let tmp = document.getElementById("item"+ idx);
+        if(index === idx) addClass(tmp, "on");
+        else              removeClass(tmp,"on");
+    });
+  }
+  return (
+    <>
+      <div className="tab-container">
+          <div className="tab-header mb15">
+            {menuArr.map((data,index) => (
+              <label className="tab" key={"tab"+index}>
+                <input type="radio"
+                       radioGroup="tab" 
+                       className="tab"
+                       id={"tab"+index}
+                       onChange={(e)=> fnTabOnChange(index, e)}
+                       value={"item"+index}
+                       name="group"/>
+                {data.menu}
+              </label>
+            ))}
+            <div className="underline" style={{width : underlineWidth+"px", left: underlineXpos}} id="underline"/>
+          </div>
+          <div></div>
+          {menuArr.map((data,index) => (
+              <div className={index===0 ? "item on":"item"} 
+                   id={"item"+index} key={"item"+index}>
+                {data.content}
+              </div>
+          ))}
+        </div>
+    </>
+  );
+};
+
+export default TabController;
--- client/src/pages/myPage/MyPage.jsx
+++ client/src/pages/myPage/MyPage.jsx
@@ -1,6 +1,7 @@
 import "css/myPage.css";
 import Bookmark from "./BookMark";
-import {toggleClass, addClass, removeClass} from "comm/util";
+import TabController from "component/TabController";
+
 
 const MyPage = () => {
   //메뉴 목록 Array  header명           ID 값             실제 Content
@@ -8,43 +9,11 @@ const MyPage = () => {
                    {menu : "북마크",    bodyId : "item1", content: <Bookmark />},
                    {menu : "테스트",    bodyId : "item2", content: <p className="pin">저장된여행글</p>  },
                   ];
-
-  /*탭 onChange 이벤트*/
-  const fnTabOnChange = (index) => {
-    menuArr.forEach((arr,idx) => {
-        let tmp = document.getElementById(arr.bodyId);
-        if(index === idx) addClass(tmp, "on");
-        else              removeClass(tmp,"on");
-    });
-  }
-
   return (
     <div className="main-frame my-page">
       <div className="left"></div>
       <div className="center">
-        <div className="tab-container">
-          <div className="tab-header mb15">
-            {menuArr.map((data,index) => (
-              <label className="tab" key={"tab"+index}>
-                <input type="radio"
-                       radioGroup="tab" 
-                       className="tab"
-                       id={"tab"+index}
-                       onChange={()=> fnTabOnChange(index)}
-                       value={data.bodyId}
-                       name="group"/>
-                {data.menu}
-              </label>
-            ))}
-          </div>
-          <div></div>
-          {menuArr.map((data,index) => (
-              <div className={index===0 ? "item on":"item"} 
-                   id={data.bodyId} key={data.bodyId}>
-                {data.content}
-              </div>
-          ))}
-        </div>
+        <TabController menuList={menuArr}/>
       </div>
       <div className="right"></div>
       
