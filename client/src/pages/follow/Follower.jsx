78ddbb13324577913021779156295d9f15ec557a
nayeon <seo8396@naver.com>
2024-05-26 21:55:50 +0900

    팔로우,팔로워 프론트엔드 커밋

---
+++ client/src/pages/follow/Follower.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import FollowContent from "./FollowContent";
+
+const Follower = () => {
+    return (
+      <>
+        <div className='follow'>
+          <FollowContent/>
+        </div>
+      </>
+    );
+};
+
+export default Follower;
